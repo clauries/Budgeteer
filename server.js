11d57286c65550076fead40de7595a7503525cf0
clauries <55369403+clauries@users.noreply.github.com>
2020-03-10 21:33:01 -0700

    Added procfile

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://claurie:si11y#b0y@ds219078.mlab.com:19078/heroku_hdsdbrwx", {
+  useMongoClient: true,
   useNewUrlParser: true,
   useFindAndModify: false
 });
